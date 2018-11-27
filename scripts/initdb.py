d8ad21ab32881abb3237f3b69a5b5f9594aac04b
mac523 <mac523@cs.bham.ac.uk>
2018-11-27 13:05:10 +0000

    implemented database connection interface class

--- scripts/initdb.py
+++ scripts/initdb.py
@@ -2,7 +2,7 @@
 # Warning: this will delete everything in the database
 import sqlite3
 
-conn = sqlite3.connect('database.db')
+conn = sqlite3.connect('../database.db')
 
 c = conn.cursor()
 
@@ -33,7 +33,8 @@ create_student_query = """CREATE TABLE {}(
 
 create_lecturer_query = """CREATE TABLE {}(
                             id INTEGER PRIMARY KEY,
-                            name TEXT
+                            name TEXT,
+                            email TEXT
 )
 """.format(LECTURER_TABLE_NAME)
 
@@ -57,26 +58,39 @@ create_enrollment_query = """CREATE TABLE {}(
 
 create_lecture_query = """CREATE TABLE {}(
                             id INTEGER PRIMARY KEY,
-                            time TEXT NOT NULL,
-                            day TEXT NOT NULL,
-                            duration INTEGER NOT NULL DEFAULT 60,
+                            starttime DATETIME NOT NULL,
+                            day DATETIME NOT NULL,
+                            endtime DATETIME NOT NULL,
+                            location TEXT NOT NULL,
                             moduleid INTEGER NOT NULL,
                             FOREIGN KEY(moduleid) REFERENCES {}(id) ON DELETE CASCADE ON UPDATE CASCADE
 )
 """.format(LECTURE_TABLE_NAME, MODULE_TABLE_NAME)
+
+create_missed_query = """CREATE TABLE {}(
+                            id INTEGER PRIMARY KEY,
+                            lectureid INTEGER NOT NULL,
+                            studentid INTEGER NOT NULL,
+                            datetime DATETIME DEFAULT current_timestamp,
+                            FOREIGN KEY(lectureid) REFERENCES {}(id) ON DELETE CASCADE ON UPDATE CASCADE,
+                            FOREIGN KEY(studentid) REFERENCES {}(id) ON DELETE CASCADE ON UPDATE CASCADE
+)
+""".format(MISSED_LEC_TABLE_NAME, LECTURE_TABLE_NAME, STUDENT_TABLE_NAME)
 
 c.execute(create_student_query)
 c.execute(create_lecturer_query)
 c.execute(create_module_query)
 c.execute(create_enrollment_query)
 c.execute(create_lecture_query)
+c.execute(create_missed_query)
 
 # Some test data
 c.execute("INSERT INTO %s(id, name) VALUES (1549223, 'Matt');" % STUDENT_TABLE_NAME)
-c.execute("INSERT INTO %s(name) VALUES ('Mohan');" % LECTURER_TABLE_NAME)
+c.execute("INSERT INTO %s(name, email) VALUES ('Mohan', 'mohan@mohan.com');" % LECTURER_TABLE_NAME)
 c.execute("INSERT INTO %s(name, lecturerid) VALUES ('Intelligent Robotics', 1);" % MODULE_TABLE_NAME)
 c.execute("INSERT INTO %s(studentid, moduleid) VALUES (1549223, 1);" % ENROLLMENT_TABLE_NAME)
-c.execute("INSERT INTO %s(time, day, moduleid) VALUES ('14:00', 'Mon', 1);" % LECTURE_TABLE_NAME)
+c.execute("INSERT INTO %s(starttime, day, endtime, location, moduleid) VALUES ('02:00', '2', '03:00', 'Room 101',1);" % LECTURE_TABLE_NAME)
+c.execute("INSERT INTO %s(starttime, day, endtime, location, moduleid) VALUES ('00:00', '2', '01:00', 'Room 101',1);" % LECTURE_TABLE_NAME)
 
 conn.commit()
 conn.close()
--- src/db_interface.py
+++ src/db_interface.py
@@ -1,16 +1,52 @@
-def getStudentCurrentLecture(studentID):
-    return # ID of lecture that student should be in, if any
+import sqlite3
 
-def storeAbsence(studentID, lectureID):
-    # Make a note in the database that the student was not at this lecture
-    pass
+class DB_Interface:
+    def __init__(self):
+        self.conn = sqlite3.connect('../database.db', isolation_level=None)
+        self.c = self.conn.cursor()
 
-def getLectureNameAndLocation(lectureID):
-    return # Name and location of lecture from DB
+    def __del__(self):
+        self.conn.close()
 
-def storeNewStudent(studentID, eigenface):
-    # Store a new student, with their face, in the DB
-    pass
+    def getStudentCurrentLecture(self, studentID):
+        # ID of lecture that student should be in, if any
+        self.c.execute("""
+                SELECT id
+                FROM Lecture
+                WHERE moduleid IN (
+                    SELECT moduleid
+                    FROM enrollment
+                    WHERE studentid = ?
+                ) AND strftime('%w', 'now') = day
+                AND strftime('%H:%M','now') BETWEEN starttime AND endtime
+                """, (studentID,))
+        return self.c.fetchone()
 
-def getNewFaces(existingStudentIDs):
-    return # (studentID, eigenface) pairs from database that are not already in the cache
+    def storeAbsence(self, studentID, lectureID):
+        # Make a note in the database that the student was not at this lecture
+        self.c.execute("INSERT INTO Missed(lectureid, studentid) VALUES (?,?)", (lectureID, studentID))
+        pass
+
+    def getLectureNameAndLocation(self, lectureID):
+        # Name and location of lecture from DB
+        self.c.execute("""
+                    SELECT location, name FROM Lecture   
+                    JOIN Module
+                    ON Lecture.moduleid = Module.id
+                    WHERE Lecture.id=?
+                    """, (lectureID,))   
+        return self.c.fetchone()
+
+    def storeNewStudent(self, studentID, eigenface):
+        # Store a new student, with their face, in the DB
+        
+        pass
+
+    def getNewFaces(self, existingStudentIDs):
+        return # (studentID, eigenface) pairs from database that are not already in the cache
+
+
+if __name__ == "__main__":
+    db = DB_Interface()
+    print(db.getLectureNameAndLocation(1))
+    print(db.getStudentCurrentLecture(1549223))
