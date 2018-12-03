5095559b81eb001ef94cc5d9d0a8d25d4287ed50
George Taylor <george.h.taylor1@gmail.com>
2018-12-03 22:50:47 +0000

    updates to init shit

--- src/speech_ui.py
+++ src/speech_ui.py
@@ -1,26 +1,24 @@
 import rospy
-import roslib
-roslib.load_manifest("sound_play")
-from sound_play.libsoundplay import SoundClient
+import os
 
 class SpeechView:
     def __init__(self):
-        rospy.init_node('say', anonymous=True)
+
         self.presenter = None
-        self.soundHandle = SoundClient()
 
     def greeting(self, name):
-        self.soundHandle.say("hello in {}".format(name))
+        self.os.system("say \"hello {}\"".format(name))
         
     def setPresenter(self, presenter):
         self.presenter = presenter
 
     def deliverNag(self, lectureName, location):
-        self.soundHandle.say("You should be in {} at {}".format(lectureName, location))
-        print("You should be in {} at {}".format(lectureName, location))
+        msg = "You should be in {} at {}".format(lectureName, location)
+        print(msg)
+        os.system("say \"{}\"".format(msg))
 
     def promptForID(self):
-        self.soundHandle.say("please enter your i d")
+        os.system("say \"please enter your i d\"")
         idInput = raw_input("Please enter your id")
         self.presenter.notifyIDSubmitted(idInput)
 
@@ -28,4 +26,5 @@ class SpeechView:
         pass
 
     def warn(self, warning):
+        os.system("say \"{}\"".format(warning))
         print("Warning in UI: {}".format(warning))
